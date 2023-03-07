55303b82649525fe20677e0db9eb3d8a0f6570e5
thienFuoc <dangthienphuoc24@gmail.com>
2023-03-07 15:21:08 +0700

    upload source code

--- public/app.js
+++ public/app.js
@@ -2,11 +2,16 @@ const imgDesktop = document.getElementById("imgDesktop");
 const imgMobile = document.getElementById("imgMobile");
 const purpleDesktop = document.getElementById("purpleDesktop");
 const purpleMobile = document.getElementById("purpleMobile");
+const slideImage = document.getElementById("slide-image");
 
 window.addEventListener("scroll", function () {
   let value = window.scrollY;
-  imgDesktop.style.left = -(value / 20 ) + 10 + "%";
+  imgDesktop.style.left = -(value / 20) + 10 + "%";
   purpleDesktop.style.right = -(value / 15) + 80 + "%";
   imgMobile.style.left = -(value / 20) + 10 + "%";
   purpleMobile.style.right = -(value / 15) + 75 + "%";
+});
+
+slideImage.addEventListener("click", function () {
+  window.location.href = "../public/about.html";
 });
